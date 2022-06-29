902abcc908ae566faf9ac8068f8422192f75aec1
Godand <aezeigwe2003@gmail.com>
2022-06-29 22:13:37 +0000

    mpkey

--- hdwallet.js
+++ hdwallet.js
@@ -1,19 +1,24 @@
-var HDKey = require('hdkey')
-var BIP39 = require('bip39');
-//require('ethutil');
+const bip39 = require('bip39')
+const hdkey = require('hdkey');
+const ethUtil = require('ethereumjs-util');
 
-const mnemonic = BIP39.generateMnemonic(); //generates string
+const mnemonic = bip39.generateMnemonic(); //generates string
 console.log(mnemonic);
-const seed = BIP39.mnemonicToSeed(mnemonic); //creates seed buffer
-console.log(seed);
-// generate master private key
-const root = HDKey.fromMasterSeed(seed);
-const masterPrivateKey = root.privateKey.toString('hex');
-console.log(masterPrivateKey);
+bip39.mnemonicToSeed(mnemonic)
+  .then(seed => {
+    console.log('Seed: ', seed);
+    console.log('mnemonic: ', mnemonic);
+
+    const root = hdkey.fromMasterSeed(seed);
+    const masterPrivateKey = root.privateKey.toString('hex');
+    console.log('mpkey' , masterPrivateKey);
+
+    const addrNode = root.derive("m/44'/60'/0'/0/0"); //line 1
+    const pubKey = ethUtil.privateToPublic(addrNode._privateKey);
+    const addr = ethUtil.publicToAddress(pubKey).toString('hex');
+    console.log('addr', addr);
+    const address = ethUtil.toChecksumAddress(addr);
+    console.log('address', address);
+  })
+
 //generate address node and address
-const addrNode = root.derive("m/44'/60'/0'/0/0"); //line 1
-const pubKey = ethUtil.privateToPublic(addrNode._privateKey);
-const addr = ethUtil.publicToAddress(pubKey).toString('hex');
-console.log(addr);
-const address = ethUtil.toChecksumAddress(addr);
-console.log(address);
