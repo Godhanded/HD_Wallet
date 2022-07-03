95209f163181afb6fbec11908586f3e236e02497
Godand <aezeigwe2003@gmail.com>
2022-07-03 19:13:07 +0000

    using ethers.js

--- EthersHDwallet.js
+++ EthersHDwallet.js
@@ -1,15 +1,22 @@
+const bip39 = require('bip39');
 const ethers = require('ethers');
-
+// const hdkey = require('hdkey');
+// const ethUtil = require('ethereumjs-util');
+let count = false;
 console.log();
+let mnemonic;
 
-function generateMnemonic() {
-    let randomEntropyBytes = ethers.utils.randomBytes(16);
-    return ethers.utils.HDNode.entropyToMnemonic(randomEntropyBytes);
-}
-
-function generateRandomHDNode() {
-    let mnemonic = generateMnemonic();
-    return ethers.utils.HDNode.fromMnemonic(mnemonic);
+function generateRandomHDNode() 
+{
+    if(!count)
+    {
+    const mnemonics= bip39.generateMnemonic(256);
+    mnemonic = mnemonics;
+    let masterNode=ethers.utils.HDNode.fromMnemonic(mnemonics);console.log(masterNode);
+    return masterNode.privateKey;
+    }else{
+        return 'already have a node';
+    }
 }
 
 console.log(generateRandomHDNode());
